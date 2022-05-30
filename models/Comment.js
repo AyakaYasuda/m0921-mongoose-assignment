293a60abfdd610155283ba29e820f97894457046
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-05-30 10:56:22 -0700

    added comment schema

---
+++ models/Comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema, model, SchemaTypes } = mongoose;
+
+const commentSchema = new Schema({
+  user: {
+    type: SchemaTypes.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: String,
+});
+
+const Comment = model("Comment", commentSchema);
+module.exports = Comment;
